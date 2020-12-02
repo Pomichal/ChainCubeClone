b3905394508ead85366b9e663170885421727672
Matej Rábek <matej.rabek@gmail.com>
2020-12-02 17:19:39 +0100

    ADD: cube prefab movement and spawning

---
+++ Assets/Scripts/Behaviours/CubeBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    public bool IsDragable = true;
+     
+    void OnMouseDown()
+    {
+        if(IsDragable)    // Only do if IsDraggable == true
+        {
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+         
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+    }
+     
+    void OnMouseDrag()
+    {
+        if(IsDragable)    // Only do if IsDraggable == true
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); // hardcode the y and z for your use
+             
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = new Vector3(Mathf.Clamp(curPosition.x, -3.45f, 3.45f), transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (IsDragable)
+        {
+            IsDragable = false;
+            GetComponent<Rigidbody>().AddForce(new Vector3(0,0,25f), ForceMode.Impulse);
+            App.gameManager.onCubeReleased.Invoke();
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-
+    public GameObject cubePrefab;
+    public UnityEvent onCubeReleased = new UnityEvent();
     void Start()
     {
         App.gameManager = this;
         App.screenManager.Show<MenuScreen>();
 
         App.player = new PlayerModel();      // TODO: load player data
+        onCubeReleased.AddListener(() =>
+        {
+            StartCoroutine("SpawnCube");
+        });
     }
 
     public void PrepareLevel()
     {
         Debug.Log("Preparing the level...");
+    }
+
+
+    IEnumerator SpawnCube()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(cubePrefab, new Vector3(0, 1, -5), Quaternion.identity);
     }
 }
